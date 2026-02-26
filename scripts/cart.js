e9a72dd75b6b024585abdd2e332e9d967a65ef90
kachy <kachigirl54@gmail.com>
2026-02-26 15:05:07 +0100

    load products from the backend

--- scripts/cart.js
+++ scripts/cart.js
@@ -11,7 +11,7 @@ cart = [{
     quantity : 2,
     deliveryOptionsId:'1'
 }, {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85b6d',
+    productId: '3ebe75dc-64d2-4137-8860-1f5a963e534b',
     quantity : 1,
     deliveryOptionsId: '3'
 
@@ -28,6 +28,7 @@ export function calculateCartQuantity(){
 
         cart.forEach((cartItem)=>{
           cartQuantity += cartItem.quantity
+        
  })
 
  return cartQuantity
